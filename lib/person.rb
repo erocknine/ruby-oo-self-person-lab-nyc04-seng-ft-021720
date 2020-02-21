df2811af4574ea66e90ca1823373546dddc6fc77
Sir Eric <echeungsk@gmail.com>
2020-02-21 14:09:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,49 @@
-# your code goes here
+
+class Person
+  attr_accessor :bank_account
+  attr_reader :name, :hygiene, :happiness
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def clean?
+    @hygiene > 7 ? true : false
+  end
+  
+  def happy?
+    @happiness > 7 ? true : false
+  end
+  
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+  
+  def take_bath
+    @hygiene = hygiene=(4)
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+  
+  def hygiene=(num)
+    @hygiene = num
+    @hygiene = limiter(hygiene)
+  end
+  
+  def happiness=(num)
+    @happiness = limiter(num)
+  end
+  
+  def limiter(num)
+    number = num
+    number = 10 if num > 10
+    number = 0 if num < 0
+    number
+  end
+  
+
+  
+end
